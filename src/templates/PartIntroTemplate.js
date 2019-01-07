2c600b8035ff0e152d3ba358e4819720853d8ece
Mark Laatikainen <mark.laatikainen@houston-inc.com>
2019-01-07 12:15:50 +0200

    Fix navigation bug

--- src/templates/PartIntroTemplate.js
+++ src/templates/PartIntroTemplate.js
@@ -31,7 +31,7 @@ export default function PartIntroTemplate({ data }) {
     },
   };
 
-  const navArray = navigation.split('@');
+  const navArray = navigation ? navigation.split('@') : [];
 
   return (
     <Layout>
