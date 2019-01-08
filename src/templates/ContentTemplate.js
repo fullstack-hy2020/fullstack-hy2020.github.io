df7be2c84e089238d346869bb1735b433dba2e1a
Mark Laatikainen <mark.laatikainen@houston-inc.com>
2019-01-08 09:15:25 +0200

    Add bottom border color to links

--- src/templates/ContentTemplate.js
+++ src/templates/ContentTemplate.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { graphql } from 'gatsby';
 import Parser from 'html-react-parser';
 import domToReact from 'html-react-parser/lib/dom-to-react';
-import React from 'react';
+import React, { Component } from 'react';
 
 import colors from '../colors';
 import Arrow from '../components/Arrow/Arrow';
@@ -17,130 +17,143 @@ import ReturnInfo from '../components/ReturnInfo/ReturnInfo';
 import ScrollNavigation from '../components/ScrollNavigation/ScrollNavigation';
 import { SubHeader } from '../components/SubHeader/SubHeader';
 
-export default function ContentTemplate({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
-  const { mainImage, title, subTitle, letter, part, partColor } = frontmatter;
+export default class ContentTemplate extends Component {
+  componentDidMount() {
+    const links = Array.from(document.querySelectorAll('a'));
+    const { frontmatter } = this.props.data.markdownRemark;
 
-  const colorCode = colors[partColor];
+    links.map(i => {
+      i.style = `border-color: ${colors[frontmatter.partColor]}`;
+    });
+  }
 
-  const parserOptions = {
-    replace: ({ type, name, attribs, children }) => {
-      if (type === 'tag' && name === 'picture') {
-        return (
-          <picture>
-            <img
-              style={{ borderColor: colorCode }}
-              alt="fullstack content"
-              src={children[0].attribs.src}
-            />
-          </picture>
-        );
-      } else if (type === 'tag' && name === 'pre') {
-        return <pre>{domToReact(children, parserOptions)}</pre>;
-      } else if (type === 'tag' && attribs.class === 'content') {
-        return (
-          <div className="container">
-            <ScrollNavigation
-              part={part}
-              letter={letter}
-              currentPath={frontmatter.path}
-              className="col-3"
-            />
+  render() {
+    const { markdownRemark } = this.props.data;
+    const { frontmatter, html } = markdownRemark;
+    const { mainImage, title, subTitle, letter, part, partColor } = frontmatter;
+    const colorCode = colors[partColor];
 
-            <div className="course-content col-7">
-              {domToReact(children, parserOptions)}
-            </div>
-          </div>
-        );
-      } else if (type === 'tag' && attribs.class === 'tasks') {
-        return (
-          <Banner
-            style={{
-              backgroundColor: colorCode,
-            }}
-            className="spacing spacing--after"
-          >
-            <div className="container">
-              <div
-                className="course-content col-7 push-right-3"
+    const parserOptions = {
+      replace: ({ type, name, attribs, children }) => {
+        if (type === 'tag' && name === 'picture') {
+          return (
+            <picture>
+              <img
                 style={{ borderColor: colorCode }}
-              >
-                {children.name === 'pre' ? (
-                  <pre>domToReact(children, parserOptions)</pre>
-                ) : (
-                  domToReact(children, parserOptions)
-                )}
-              </div>
-            </div>
-          </Banner>
-        );
-      }
-      return;
-    },
-  };
-
-  return (
-    <Layout>
-      <div className="spacing--small spacing--after">
-        <div className="course-container">
-          <Banner
-            backgroundColor={colorCode}
-            className="spacing--after"
-            style={{
-              backgroundImage: `url(${path.resolve(mainImage.publicURL)})`,
-              backgroundPosition: 'center center',
-              backgroundSize: '80%',
-              backgroundRepeat: 'no-repeat',
-              backgroundColor: colorCode,
-            }}
-          >
-            <div className="container">
-              <Arrow
-                upperCase
-                content={[
-                  {
-                    backgroundColor: colorCode,
-                    text: 'Yleistä',
-                    link: '/about',
-                  },
-                  {
-                    backgroundColor: colorCode,
-                    text: title,
-                    link: `/osa${part}`,
-                  },
-                  {
-                    backgroundColor: 'black',
-                    text: subTitle,
-                  },
-                ]}
+                alt="fullstack content"
+                src={children[0].attribs.src}
               />
-            </div>
-          </Banner>
-          <div className="course">
+            </picture>
+          );
+        } else if (type === 'tag' && name === 'pre') {
+          return <pre>{domToReact(children, parserOptions)}</pre>;
+        } else if (type === 'tag' && attribs.class === 'content') {
+          return (
             <div className="container">
-              <div className="col-7 course-content push-right-3">
-                <p className="col-1 letter" style={{ borderColor: colorCode }}>
-                  {letter}
-                </p>
-                <SubHeader headingLevel="h1" text={subTitle} />
+              <ScrollNavigation
+                part={part}
+                letter={letter}
+                currentPath={frontmatter.path}
+                className="col-3"
+              />
+
+              <div className="course-content col-7">
+                {domToReact(children, parserOptions)}
               </div>
             </div>
-            {Parser(html, parserOptions)}
+          );
+        } else if (type === 'tag' && attribs.class === 'tasks') {
+          return (
+            <Banner
+              style={{
+                backgroundColor: colorCode,
+              }}
+              className="spacing spacing--after"
+            >
+              <div className="container">
+                <div
+                  className="course-content col-7 push-right-3"
+                  style={{ borderColor: colorCode }}
+                >
+                  {children.name === 'pre' ? (
+                    <pre>{domToReact(children, parserOptions)}</pre>
+                  ) : (
+                    domToReact(children, parserOptions)
+                  )}
+                </div>
+              </div>
+            </Banner>
+          );
+        }
+        return;
+      },
+    };
+
+    return (
+      <Layout>
+        <div className="spacing--small spacing--after">
+          <div className="course-container">
+            <Banner
+              backgroundColor={colorCode}
+              className="spacing--after"
+              style={{
+                backgroundImage: `url(${path.resolve(mainImage.publicURL)})`,
+                backgroundPosition: 'center center',
+                backgroundSize: '80%',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: colorCode,
+              }}
+            >
+              <div className="container">
+                <Arrow
+                  upperCase
+                  content={[
+                    {
+                      backgroundColor: colorCode,
+                      text: 'Fullstack',
+                      link: '/about',
+                    },
+                    {
+                      backgroundColor: colorCode,
+                      text: title,
+                      link: `/osa${part}`,
+                    },
+                    {
+                      backgroundColor: 'black',
+                      text: subTitle,
+                    },
+                  ]}
+                />
+              </div>
+            </Banner>
+            <div className="course">
+              <div className="container">
+                <div className="col-7 course-content push-right-3">
+                  <p
+                    className="col-1 letter"
+                    style={{ borderColor: colorCode }}
+                  >
+                    {letter}
+                  </p>
+                  <SubHeader headingLevel="h1" text={subTitle} />
+                </div>
+              </div>
+              {Parser(html, parserOptions)}
+            </div>
+
+            <ReturnInfo />
+
+            <PrevNext
+              prev={part > 0 ? part - 1 : undefined}
+              next={part < 8 ? part + 1 : undefined}
+            />
           </div>
-
-          <ReturnInfo />
-
-          <PrevNext
-            prev={part !== 0 ? part - 1 : undefined}
-            next={part !== 7 ? part + 1 : undefined}
-          />
         </div>
-      </div>
 
-      <Footer />
-    </Layout>
-  );
+        <Footer />
+      </Layout>
+    );
+  }
 }
 
 export const contentPageQuery = graphql`
