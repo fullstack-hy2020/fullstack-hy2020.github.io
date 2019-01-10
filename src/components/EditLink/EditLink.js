e51e5c407f3bbb3e07b879166b2c7f653e38241c
Mark Laatikainen <mark.laatikainen@houston-inc.com>
2019-01-10 11:44:08 +0200

    Introduce EditLink

---
+++ src/components/EditLink/EditLink.js
@@ -0,0 +1,25 @@
+import './EditLink.scss';
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Element from '../Element/Element';
+
+const EditLink = ({ part, letter }) => (
+  <Element flex className="container spacing" centered>
+    <a
+      className="edit-link"
+      target="__BLANK"
+      href={`https://github.com/fullstack-hy2019/fullstack-hy2019.github.io/edit/source/src/content/osa${part}/osa${part}${letter}.md`}
+    >
+      Muokkaa sisältöä
+    </a>
+  </Element>
+);
+
+EditLink.propTypes = {
+  part: PropTypes.string.isRequired,
+  letter: PropTypes.string.isRequired,
+};
+
+export default EditLink;
