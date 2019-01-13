06eee80e18a319c81e137695a60e160264c7af3a
Mark Laatikainen <mark.laatikainen@houston-inc.com>
2019-01-13 10:07:29 +0200

    Refactor Faq to use GraphQL and Accordion

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,36 +1,56 @@
+import { StaticQuery, graphql } from 'gatsby';
 import React from 'react';
 
+import Accordion from '../components/Accordion/Accordion';
 import Element from '../components/Element/Element';
-import { BodyText } from '../components/BodyText/BodyText';
 import Footer from '../components/Footer/Footer';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import faqs from '../content/faq/faq.json'
+import { SubHeader } from '../components/SubHeader/SubHeader';
 
 const FAQ = () => (
   <Layout>
     <SEO title="FAQ" />
 
-    <Element className="container">
-      <Element className="col-10 col-8--mobile spacing--small">
-        <BodyText
-          heading={{ title: 'Usein kysytyst kysymykset' , level: 'h1' }}
-        />
-      </Element>    
+    <StaticQuery
+      query={graphql`
+        query {
+          allFaqJson {
+            edges {
+              node {
+                title
+                text
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const { edges } = data.allFaqJson;
 
-      <h1>Usein kysytyst kysymykset</h1>
+        return (
+          <Element className="container link spacing spacing--after-small">
+            <SubHeader
+              className="spacing--small spacing--after-small"
+              headingLevel="h1"
+              text="Usein kysytyst kysymykset"
+            />
 
-      {faqs.map(faq =>
-        <Element className="col-10 col-8--mobile">
-          <BodyText
-            className="spacing--small" 
-            heading={{ title: faq.title , level: 'h3' }}
-            text={faq.text} 
-          />
-        </Element> 
-      )}
+            {edges.map(edge => {
+              const { node } = edge;
 
-    </Element>
+              return (
+                <Accordion
+                  key={node.title}
+                  title={node.title}
+                  content={node.text}
+                />
+              );
+            })}
+          </Element>
+        );
+      }}
+    />
 
     <Footer />
   </Layout>
