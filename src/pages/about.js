aa6ada4eb9787f44bb0f11e6f866fecdedc23775
Mark <mark.laatikainen@houston-inc.com>
2019-03-12 22:47:32 +0200

    Update about page styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import { StaticQuery, graphql } from 'gatsby';
 
 import Accordion from '../components/Accordion/Accordion';
-import Arrow from '../components/Arrow/Arrow';
+import { Banner } from '../components/Banner/Banner';
 import { BodyText } from '../components/BodyText/BodyText';
 import Element from '../components/Element/Element';
 import Footer from '../components/Footer/Footer';
@@ -10,7 +10,6 @@ import Layout from '../components/layout';
 import { PartBanner } from '../components/PartBanner/PartBanner';
 import React from 'react';
 import SEO from '../components/seo';
-import colors from '../colors';
 import landingImage from '../images/landing.svg';
 
 const About = () => (
@@ -35,7 +34,6 @@ const About = () => (
             edges {
               node {
                 id
-                name
                 intro
                 info {
                   title
@@ -47,54 +45,56 @@ const About = () => (
         }
       `}
       render={data => {
-        const { name, intro, info } = data.allAboutJson.edges[0].node;
+        const { intro, info } = data.allAboutJson.edges[0].node;
 
         return (
           <>
-            <Element className="container">
-              <Arrow
-                className="spacing--small"
-                content={[
-                  {
-                    backgroundColor: colors['black'],
-                    text: name,
-                  },
-                ]}
-              />
-
-              <Element flex spaceBetween>
-                <div className="col-6 spacing--after">
-                  <BodyText text={intro} className="link" headingFont />
-
-                  <BodyText
-                    className="link"
-                    headingFont
-                    text={[
-                      'Osallistujilta edellytetään vahvaa ohjelmointirutiinia, web-ohjelmoinnin ja tietokantojen perustuntemusta, git-versionhallintajärjestelmän peruskäytön hallintaa, kykyä pitkäjänteiseen työskentelyyn sekä valmiutta omatoimiseen tiedonhakuun ja ongelmanratkaisuun.',
-                      'Osallistuminen ei kuitenkaan edellytä kurssilla käsiteltävien tekniikoiden tai Javascript-kielen hallintaa.',
-                      'Kurssi sisältää yhden luennon, kohtuullisen määrään pajaohjausta ja hirveän määrän itsenäistä koodaamista. Varaudu käyttämään aikaa noin 10-15 tuntia viikossa koko periodin ajan.',
-                      'Kurssin sisällöstä ja suoritustavasta kerrotaan materiaalissa osassa 0, lue ne huolella.',
-                    ]}
-                  />
-                </div>
-
+            <Banner style={{ paddingBottom: 0 }}>
+              <div
+                style={{ display: 'flex', justifyContent: 'center' }}
+                className="container"
+              >
                 <Image
+                  className="col-4 push-right-2"
                   contain
                   style={{ margin: 0 }}
                   alt="Stacked cubes with React logo and JavaScript text"
                   src={landingImage}
                 />
-              </Element>
+              </div>
+            </Banner>
 
-              {false &&
-                info.map(item => (
-                  <Accordion
-                    key={item}
-                    title={item.title}
-                    content={item.content}
-                  />
-                ))}
+            <Element className="container spacing">
+              <Element className="col-7 push-right-3">
+                <BodyText heading={{ level: 'h1', title: 'Yleistä' }} />
+
+                <Element flex spaceBetween>
+                  <div className="col-6 spacing--after">
+                    <BodyText text={intro} className="link" headingFont />
+
+                    <BodyText
+                      className="link"
+                      headingFont
+                      text={[
+                        'Osallistujilta edellytetään vahvaa ohjelmointirutiinia, web-ohjelmoinnin ja tietokantojen perustuntemusta, git-versionhallintajärjestelmän peruskäytön hallintaa, kykyä pitkäjänteiseen työskentelyyn sekä valmiutta omatoimiseen tiedonhakuun ja ongelmanratkaisuun.',
+                        'Osallistuminen ei kuitenkaan edellytä kurssilla käsiteltävien tekniikoiden tai Javascript-kielen hallintaa.',
+                        'Kurssi sisältää yhden luennon, kohtuullisen määrään pajaohjausta ja hirveän määrän itsenäistä koodaamista. Varaudu käyttämään aikaa noin 10-15 tuntia viikossa koko periodin ajan.',
+                        'Kurssin sisällöstä ja suoritustavasta kerrotaan materiaalissa osassa 0, lue ne huolella.',
+                      ]}
+                    />
+                  </div>
+                </Element>
+              </Element>
             </Element>
+
+            {false &&
+              info.map(item => (
+                <Accordion
+                  key={item}
+                  title={item.title}
+                  content={item.content}
+                />
+              ))}
 
             <PartBanner />
           </>
