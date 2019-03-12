21a4577fc23c2aa0e4cc3cc735ca1cdca6b647eb
Mark <mark.laatikainen@houston-inc.com>
2019-03-12 23:29:12 +0200

    Add read more button to stories

--- src/components/DeveloperStory/DeveloperStory.js
+++ src/components/DeveloperStory/DeveloperStory.js
@@ -1,39 +1,71 @@
 import './DeveloperStory.scss';
 
+import React, { Component } from 'react';
+
 import { BodyText } from '../BodyText/BodyText';
 import Element from '../Element/Element';
-import {Image} from '../Image/Image';
+import { Image } from '../Image/Image';
 import { PropTypes } from 'prop-types';
-import React from 'react';
 import { TripleBorder } from '../TripleBorder/TripleBorder';
 
-export const DeveloperStory = ({ image, companyLogo, text, name, title }) => (
-  <Element
-    flex
-    spaceBetween
-    className="col-8 col-10--mobile push-right-1 spacing--small spacing--after"
-  >
-    <TripleBorder largeMargin className="developer-story__image col-3 col-7--mobile">
-      <Image squareBig src={require(`../../images/${image}`)} alt={name} />
-    </TripleBorder>
+export class DeveloperStory extends Component {
+  constructor(props) {
+    super(props);
 
-    <Element className="col-6 col-8--mobile spacing--mobile">
-      {companyLogo && (
-        <img
-          className="col-2 col-5--mobile"
-          src={require(`../../images/${companyLogo}`)}
-          alt={companyLogo.split('.')[0]}
-        />
-      )}
+    this.state = {
+      readMore: false,
+    };
+  }
 
-      <BodyText headingFont className="col-10" text={text} />
+  render() {
+    const { image, companyLogo, text, name, title } = this.props;
+    const { readMore } = this.state;
 
-      <p className="col-10 developer-story__name">{name}</p>
+    return (
+      <Element
+        flex
+        spaceBetween
+        className="col-8 col-10--mobile push-right-1 spacing--small spacing--after"
+      >
+        <TripleBorder
+          largeMargin
+          className="developer-story__image col-3 col-7--mobile"
+        >
+          <Image squareBig src={require(`../../images/${image}`)} alt={name} />
+        </TripleBorder>
 
-      <p className="col-10 developer-story__title">{title}</p>
-    </Element>
-  </Element>
-);
+        <Element className="col-6 col-8--mobile spacing--mobile">
+          {companyLogo && (
+            <img
+              className="col-2 col-5--mobile"
+              src={require(`../../images/${companyLogo}`)}
+              alt={companyLogo.split('.')[0]}
+            />
+          )}
+
+          <BodyText
+            headingFont
+            className="col-10"
+            text={readMore ? text : text[0]}
+          />
+
+          {!readMore && (
+            <button
+              onClick={() => this.setState({ readMore: true })}
+              className="col-10 developer-story__read-more"
+            >
+              Lue lisää
+            </button>
+          )}
+
+          <p className="col-10 developer-story__name">{name}</p>
+
+          <p className="col-10 developer-story__title">{title}</p>
+        </Element>
+      </Element>
+    );
+  }
+}
 
 DeveloperStory.defaultProps = {
   image: null,
