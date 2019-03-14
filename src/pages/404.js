dc28b53523b8614714d7fa5cf668688feff2e927
Mark <mark.laatikainen@houston-inc.com>
2019-03-14 17:15:47 +0200

    Fix spacing

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,10 +13,10 @@ const NotFoundPage = () => (
     <Element className="container spacing--large spacing--after">
       <h1>404 - Sivua ei löytynyt</h1>
 
-      <p className="col-10 spacing--small">Uncaught ReferenceError: unknown is not defined</p>
+      <p className="col-10 spacing--small spacing--after">Uncaught ReferenceError: unknown is not defined</p>
 
       <Arrow
-        className="col-10 spacing arrow__container--with-link"
+        className="col-10 arrow__container--with-link"
         bold
         thickBorder
         link="/"
