4d076c244f7ad49d7e891cf546a10f7fa144146f
Mark <mark.laatikainen@houston-inc.com>
2019-03-14 16:26:32 +0200

    Edit link style changes

--- src/components/EditLink/EditLink.js
+++ src/components/EditLink/EditLink.js
@@ -1,9 +1,8 @@
 import './EditLink.scss';
 
+import Element from '../Element/Element';
 import PropTypes from 'prop-types';
 import React from 'react';
-
-import Element from '../Element/Element';
 
 const EditLink = ({ part, letter }) => (
   <Element flex className="container spacing" centered>
@@ -12,7 +11,7 @@ const EditLink = ({ part, letter }) => (
       target="__BLANK"
       href={`https://github.com/fullstackopen-2019/fullstackopen-2019.github.io/edit/source/src/content/osa${part}/osa${part}${letter}.md`}
     >
-      Ehdota muutosta materiaalin sisältöön
+      <span>Ehdota muutosta materiaalin sisältöön</span>
     </a>
   </Element>
 );
