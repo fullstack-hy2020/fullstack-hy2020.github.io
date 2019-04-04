d0fd0b2fa6b2fb47faaa5b169b142823b69e2ae8
Mark <mark.laatikainen@houston-inc.com>
2019-04-04 14:51:49 +0300

    Fix z-index

--- src/components/TripleBorder/TripleBorder.js
+++ src/components/TripleBorder/TripleBorder.js
@@ -29,7 +29,7 @@ export const TripleBorder = ({
   >
     <div
       className={`triple-border__container ${childrenClassName}`}
-      style={{ backgroundColor: backgroundColor, zIndex: 9999999999 }}
+      style={{ backgroundColor: backgroundColor, zIndex: 10 }}
     >
       {children}
     </div>
