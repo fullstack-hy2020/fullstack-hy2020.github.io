6f513146aad3037e25031afb2c8c2b229abab888
Matti Luukkainen <mluukkai@iki.fi>
2019-05-28 22:56:47 +0300

    parts in progress commented out

--- src/content/partnavigation/partnavigation.js
+++ src/content/partnavigation/partnavigation.js
@@ -68,49 +68,63 @@ module.exports = {
       d: 'A more complex state, debugging React apps',
     },
     '2': {
+      /*
       a: 'Rendering a collection, modules',
       b: 'Forms',
       c: 'Getting data from server',
       d: 'Altering data in server',
       e: 'Adding styles to React app',
+      */
     },
     '3': {
+      /*
       a: 'Node.js and Express',
       b: 'Deploying app to internet',
       c: 'Saving data to MongoDB',
       d: 'Validation and ESLint',
+      */
     },
     '4': {
+      /*
       a: 'Structure of backend application, introduction to testing',
       b: 'Testing the backend',
       c: 'User administration',
       d: 'Token authentication'
+      */
     },
     '5': {
+      /*
       a: 'Login in frontend',
       b: 'props.children and proptypes',
       c: 'Testing React apps',
       d: 'Custom hooks'
+      */
     },
     '6': {
+      /*
       a: 'Flux-arkkitecture and Redux',
       b: 'Many redusers, connect',
       c: 'Communicating with server in a redux application',
+      */
     },
     '7': {
+      /*
       a: 'React-router',
       b: 'More about styles',
       c: 'Webpack',
       d: 'Class components, E2E-testing',
       e: 'Miscellaneous',
       f: 'Exercises: extending the blogilist',
+      */
     },
     '8': {
+      /*
       a: 'GraphQL-serves',
       b: 'React and GraphQL',
       c: 'Database and user administration',
       d: 'Login and updating the cache',
       e: 'Fragments and subscriptions'
+      */
     },
   },
 };
