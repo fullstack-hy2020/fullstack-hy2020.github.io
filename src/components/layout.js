e2162777980caee925450f8a959bee764beea350
Mark <mark.laatikainen@houston-inc.com>
2019-05-31 13:16:37 +0300

    Add release message

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,48 @@
 import './layout.css';
 import './index.scss';
 
+import React, { Component } from 'react';
+
+import EnglishRelease from './EnglishRelease/EnglishRelease';
 import Header from './Header/Header';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
 class Layout extends Component {
   state = {
-    siteLanguage: 'fi'
-  }
+    siteLanguage: 'fi',
+    notificationDismissed: false,
+  };
 
   componentDidMount() {
     const siteLanguage =
       window.location.pathname.indexOf('/en') !== -1 ? 'en' : 'fi';
 
-    this.setState({ siteLanguage });
+    const messageDismissed = window.localStorage.getItem(
+      'notificationDismissed'
+    );
+
+    this.setState({
+      siteLanguage: siteLanguage,
+      notificationDismissed: messageDismissed,
+    });
+  }
+
+  dismissMessage = bool => {
+    this.setState({ notificationDismissed: bool }, () => {
+      window.localStorage.setItem('notificationDismissed', bool);
+    });
   };
 
   render() {
+    const { siteLanguage, notificationDismissed } = this.state;
+
     return (
       <div className="main-wrapper">
         <Header lang={this.state.siteLanguage} />
+
+        {siteLanguage === 'fi' && !notificationDismissed && (
+          <EnglishRelease dismissed={bool => this.dismissMessage(bool)} />
+        )}
 
         {this.props.children}
       </div>
