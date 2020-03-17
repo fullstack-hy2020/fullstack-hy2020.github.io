0338593fefb4a8f1f5eb8a8880241763e4163194
RichardStark <yantaiv@gmail.com>
2020-03-17 21:19:00 +0800

    challenge complete

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -85,123 +85,58 @@ class Form extends Component {
       question,
     } = this.state;
 
-    const { lang } = this.props
+    const { lang } = this.props;
 
-    return (
-      <>
-        {showForm ? (
-          <>
-            {!formIsSent ? (
-              <form onSubmit={this.handleSubmit} className="form col-10">
+    return <>
+        {showForm ? <>
+            {!formIsSent ? <form onSubmit={this.handleSubmit} className="form col-10">
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Nimi' : 'Name'}
+                  {lang === 'fi' ? 'Nimi' : lang === 'zh' ? '姓名' : 'Name'}
                   <span aria-hidden="true">*</span>
                 </p>
-                <input
-                  required
-                  autoComplete="off"
-                  className="col-10"
-                  placeholder={lang === 'fi' ? "Matti Meikäläinen" : "Jane Doe"}
-                  type="text"
-                  name="name"
-                  value={name}
-                  onChange={this.handleChange}
-                />
+                <input required autoComplete="off" className="col-10" placeholder={lang === 'fi' ? 'Matti Meikäläinen' : lang === 'zh' ? '小明' : 'Jane Doe'} type="text" name="name" value={name} onChange={this.handleChange} />
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Titteli' : 'Title'}
+                  {lang === 'fi' ? 'Titteli' :lang === 'zh' ? '职位' : 'Title'}
                   <span aria-hidden="true">*</span>
                 </p>
-                <input
-                  required
-                  autoComplete="off"
-                  className="col-10"
-                  placeholder="CEO"
-                  type="text"
-                  name="title"
-                  value={title}
-                  onChange={this.handleChange}
-                />
+                <input required autoComplete="off" className="col-10" placeholder="CEO" type="text" name="title" value={title} onChange={this.handleChange} />
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Yritys' : 'Company'}
+                  {lang === 'fi' ? 'Yritys' :lang === 'zh' ? '公司名称' : 'Company'}
                   <span aria-hidden="true">*</span>
                 </p>
-                <input
-                  required
-                  autoComplete="off"
-                  className="col-10"
-                  placeholder={lang === 'fi' ? "Yritys Oy" : "Company Inc"}
-                  type="text"
-                  name="organization"
-                  value={organization}
-                  onChange={this.handleChange}
-                />
+                <input required autoComplete="off" className="col-10" placeholder={lang === 'fi' ? 'Yritys Oy' : lang === 'fi' ? 'Yritys Oy' : '股份有限公司'} type="text" name="organization" value={organization} onChange={this.handleChange} />
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Puhelinnumero' : 'Phone'}
+                  {lang === 'fi' ? 'Puhelinnumero' : lang === 'zh' ? '电话' : 'Phone'}
                   <span aria-hidden="true">*</span>
                 </p>
-                <input
-                  autoComplete="off"
-                  className="col-10"
-                  placeholder="+358 40 234 5678"
-                  type="text"
-                  name="phone"
-                  value={phone}
-                  onChange={this.handleChange}
-                />
+                <input autoComplete="off" className="col-10" placeholder="+358 40 234 5678" type="text" name="phone" value={phone} onChange={this.handleChange} />
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Sähköpostiosoite' : 'email address'}
+                  {lang === 'fi' ? 'Sähköpostiosoite' :lang === 'zh' ? '电子邮箱' : 'email address'}
                   <span aria-hidden="true">*</span>
                 </p>
-                <input
-                  required
-                  autoComplete="off"
-                  className="col-10"
-                  placeholder="email@domain.com"
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={this.handleChange}
-                />
+                <input required autoComplete="off" className="col-10" placeholder="email@domain.com" type="email" name="email" value={email} onChange={this.handleChange} />
                 <p className="spacing--small">
-                  {lang === 'fi' ? 'Mistä kuulit Full Stack -haasteesta?' : 'Where did you hear from the challenge'} 
+                  {lang === 'fi'
+                    ? 'Mistä kuulit Full Stack -haasteesta?'
+                    :lang === 'zh'
+                    ? '您从哪里听说的本次挑战'
+                    : 'Where did you hear from the challenge'}
                 </p>
-                <input
-                  autoComplete="off"
-                  className="col-10"
-                  type="text"
-                  name="question"
-                  value={question}
-                  onChange={this.handleChange}
-                />
-                <button
-                  className="submit spacing spacing--after push-right-4"
-                  type="submit"
-                >
-                  {lang === 'fi' ? 'Lähetä' : 'Submit'}
+                <input autoComplete="off" className="col-10" type="text" name="question" value={question} onChange={this.handleChange} />
+                <button className="submit spacing spacing--after push-right-4" type="submit">
+                  {lang === 'fi' ? 'Lähetä' :lang === 'zh' ? '提交' : 'Submit'}
                 </button>
-              </form>
-            ) : (
-              <BodyText
-                className="spacing"
-                headingFont
-                  text={[lang === 'fi' ? 'Kiitos! Otamme sinuun yhteyttä!' : 'Thanks! We will contact you soon!']}
-              />
-            )}
-          </>
-        ) : (
-          <>
+              </form> : <BodyText className="spacing" headingFont text={[lang === 'fi' ? 'Kiitos! Otamme sinuun yhteyttä!' : lang === 'zh' ? '谢谢，我们会尽快联系您' :'Thanks! We will contact you soon!']} />}
+          </> : <>
             <Element flex spaceAround className="col-10 spacing">
-              <button
-                className="about__challenge-button about__challenge-button--turquoise"
-                onClick={() => this.setState({ showForm: true })}
-              >
-                {lang ==='fi' ? 'Ilmoita yrityksesi mukaan haasteeseen!' : 'Register your company to Full stack challenge'}
+              <button className="about__challenge-button about__challenge-button--turquoise" onClick={() => this.setState(
+                    { showForm: true }
+                  )}>
+                {lang === 'fi' ? 'Ilmoita yrityksesi mukaan haasteeseen!' : lang === 'zh' ? '将您的公司注册到全栈挑战中' : 'Register your company to Full stack challenge'}
               </button>
             </Element>
-          </>
-        )}
-      </>
-    );
+          </>}
+      </>;
   }
 }
 
--- src/pages/challenge.zh.js
+++ src/pages/challenge.zh.js
@@ -14,8 +14,8 @@ import mainSEOtags from '../content/seo/mainSEOtags';
 const Challenge = () => (
   <Layout>
     <SEO
-      lang="en"
-      title="Full stack -challenge | Full stack open 2020"
+      lang="zh"
+      title="全栈——挑战 | 全栈公开课2020"
       description="The University of Helsinki and Houston Inc. have partnered up to challenge both individual developers and companies alike to learn and build upon their expertise in true Full Stack development. The course is built by coders for coders, and offers something for both newcomers and seasoned industry veterans alike. "
       keywords={[
         ...mainSEOtags,
@@ -46,7 +46,7 @@ const Challenge = () => (
     <Element className="container spacing">
       <SubHeader
         className="col-10col-8 push-right-1"
-        text="This is Full Stack -challenge"
+        text="这就是全栈！——挑战内容"
         headingLevel="h1"
       />
 
@@ -54,10 +54,10 @@ const Challenge = () => (
         <BodyText
           headingFont
           text={[
-            'The world is in dire need of software developers. We want to help breed the next generation of coders — one of our offered methods is to enroll in the Full Stack MOOC course.',
-            'The University of Helsinki and Houston Inc. have partnered up to challenge both individual developers and companies alike to learn and build upon their expertise in true Full Stack spirit. Among our affiliates and challenge initiators are Elisa Oyj, K-ryhmä, Konecranes Oyj, Terveystalo Oyj, and Unity Technologies Finland Ltd.',
-            'Our objective is to encourage learning. We offer a fully-assembled and purposefully-scoped Full Stack course that lowers the barrier of entry for learning about new state-of-the-art and production grade technologies. The course is free of charge and you can participate from anywhere at anytime, at your leisure.',
-            'The course is built by coders for coders, and offers something for both newcomers and seasoned industry veterans alike. The only prerequisite is a solid grasp of basic programming fundamentals.',
+            '这个世界迫切地需要软件开发人员。 我们希望帮助培育下一代程序员ーー我们提供的方法之一是就报名参加全栈 MOOC 公开课',
+            ' 赫尔辛基大学和Houston 公司已经结成伙伴关系，向个人开发者和公司提出挑战，要求他们学习并发扬真正的全栈精神。 我们的友情机构以及本次挑战的发起者是 Elisa Oyj，k-ryhm，Konecranes Oyj，Terveystalo Oyj，和 Unity Technologies Finland Ltd',
+            '我们的目标是鼓励学习。 我们提供了一个大而全，且目标导向的全栈课程，降低了学习这些技术的门槛，而这些内容是业界领先的，并且是生产级别的。 课程全部免费，您可以在任何时间、任何地点、在您的闲暇时间参与其中',
+            '该课程从程序员中来，到程序员中去，为新手也为经验丰富的行业老手提供一些帮助。 唯一的先决条件是扎实地掌握基本的编程基础',
           ]}
         />
 
@@ -66,24 +66,24 @@ const Challenge = () => (
           headingFont
           heading={{
             level: 'h2',
-            title: 'Companies, Heed The Call!',
+            title: '贵司，接受挑战吧！',
           }}
           text={[
-            'By accepting the challenge you are encouraging and empowering the members of your working community to acquire new skills. The fully-online Full Stack course will make a great addition to your internal training and development programs and will serve well as self-study material amongst your other in-company training offerings.',
-            'By partaking in the challenge, you will send a strong message to the outside world about your company’s technical expertise — this will help build your employer image and increase your visibility to software industry professionals.',
-            'Joining the challenge is free of charge. Companies that heed the call and accept the challenge will have their logo proudly placed on the Full Stack course webpage.',
+            '通过接受这项挑战，你就是在鼓励和赋能你的员工，获得新的技能。 完全线上的全栈课程将为您的内部培训和发展计划提供一个很好的补充，同样也可以作为您公司内部培训课程中的自学材料',
+            '通过参与这项挑战，你将向外界发出一个关于贵司技术专长的强烈信息ーー这将有助于树立你的企业形象，并提高贵司在软件行业专业人士中的知名度',
+            '参与这项挑战是免费的。 参与并接受挑战的公司应当自豪地把你们的公司LOGO放在全栈课程网页上',
           ]}
         />
 
-        <Form lang="en" />
+        <Form lang="zh" />
       </Element>
     </Element>
 
-    <CompaniesBanner lang="en" />
+    <CompaniesBanner lang="zh" />
 
     <SubHeader
       className="col-10 spacing--after centered"
-      text="#fullstackchallenge"
+      text="#全栈挑战"
       headingLevel="h3"
     />
   </Layout>
