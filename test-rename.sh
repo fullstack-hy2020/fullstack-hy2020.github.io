fff94e6e52c2d2945121bf514b244638d446b16c
RichardStark <yantaiv@gmail.com>
2020-04-02 13:30:05 +0800

    remove test file and ready merge

--- test-rename.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find ./src/content -type f|grep zh | while read i
-do
-    cp $i /tmp/part
-done
--- test-sed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find ./src/content -type f|grep zh | while read i
-do
-    sed -i  '' 's/lang: en/lang: zh/g' $i
-done
