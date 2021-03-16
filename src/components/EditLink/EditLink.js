ad553e30da58141174aaece5bce7a02c2145ef56
Kalle Ilves <kalleilv@gmail.com>
2021-03-16 14:36:42 +0200

    propose changes for spanish translation

--- src/components/EditLink/EditLink.js
+++ src/components/EditLink/EditLink.js
@@ -5,22 +5,25 @@ import { useTranslation } from 'react-i18next';
 import Element from '../Element/Element';
 import './EditLink.scss';
 
+const baseUrlByLanguage = {
+  zh: 'https://github.com/RichardStark/fullstack-hy2020.github.io',
+  es: 'https://github.com/sebastiantorres86/fullstack-hy2020.github.io',
+};
+
 const EditLink = ({ part, letter }) => {
   const { t, i18n } = useTranslation();
   const lang = i18n.language;
   const link = lang === 'fi' ? `osa${part}` : `part${part}`;
 
+  const baseUrl =
+    baseUrlByLanguage[lang] ||
+    'https://github.com/fullstack-hy2020/fullstack-hy2020.github.io';
+
+  const url = `${baseUrl}/edit/source/src/content/${part}/${lang}/${link}${letter}.md`;
+
   return (
     <Element flex className="container spacing" centered>
-      <a
-        className="edit-link"
-        target="__BLANK"
-        href={
-          lang === 'zh'
-            ? `https://github.com/RichardStark/fullstack-hy2020.github.io/edit/source/src/content/${part}/${lang}/${link}${letter}.md`
-            : `https://github.com/fullstack-hy2020/fullstack-hy2020.github.io/edit/source/src/content/${part}/${lang}/${link}${letter}.md`
-        }
-      >
+      <a className="edit-link" target="__BLANK" href={url}>
         <span>{t('proposeChanges')}</span>
       </a>
     </Element>
