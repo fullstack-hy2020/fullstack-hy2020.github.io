4bc0c98f993a31a42e716b67e3e8323301faaee1
Gabriele Masciulli <gmasciu@gmail.com>
2022-04-27 18:15:52 +0200

    Italian about page

---
+++ src/pages/about.it.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutPage from '../components/AboutPage';
+
+const About = () => {
+  return (
+    <AboutPage lang="it" title="Riguardo al corso | Full Stack open 2022" />
+  );
+};
+
+export default About;
