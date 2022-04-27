5e155c5334233ce9b97f8fc19211fa7376971c2f
Gabriele Masciulli <gmasciu@gmail.com>
2022-04-27 17:55:45 +0200

    Created italian FAQ page

---
+++ src/pages/faq.it.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FaqPage from '../components/FaqPage';
+
+const FAQ = () => (
+  <FaqPage
+    lang="it"
+    title="FAQ | Full Stack open 2022"
+    seoDescription="Domande frequenti"
+  />
+);
+
+export default FAQ;
