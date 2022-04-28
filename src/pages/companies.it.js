3dd8ed522723a80001d2d37fe26ecd2683ed6823
Gabriele Masciulli <gmasciu@gmail.com>
2022-04-28 11:46:51 +0200

    it. companies page

---
+++ src/pages/companies.it.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CompaniesPage from '../components/CompaniesPage';
+
+const Companies = () => (
+  <CompaniesPage
+    lang="it"
+    title="Aziende partner | Full Stack open 2022"
+    seoDescription="Full stack development in aziende partner"
+  />
+);
+
+export default Companies;
