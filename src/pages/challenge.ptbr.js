49b5118a93cf5a42a9259b86b4143a253500268f
Maycon Lopes <lopes.maycon@gmail.com>
2023-02-26 13:37:15 -0300

    minor textual adjustments

--- src/pages/challenge.ptbr.js
+++ src/pages/challenge.ptbr.js
@@ -7,10 +7,10 @@ const Challenge = () => {
     <ChallengePage
       lang="ptbr"
       title="Desafio Full stack | Full Stack Aberto"
-      seoDescription="A Universidade de Helsinki e a Houston Inc. se uniram para desafiar tanto desenvolvedores individuais quanto empresas a aprender e a aprimorar suas habilidades em desenvolvimento de software do jeito Full Stack. O curso é construído por programadores para programadores, e oferece valor tanto para iniciantes quanto para veteranos da indústria."
+      seoDescription="A Universidade de Helsinque e a Houston Inc. se uniram para desafiar tanto desenvolvedores individuais quanto empresas a aprender e a aprimorar suas habilidades em desenvolvimento de software do jeito Full Stack. O curso é construído por programadores para programadores, e oferece valor tanto para iniciantes quanto para veteranos da indústria."
       aboutContent={[
         'O mundo precisa desesperadamente de desenvolvedores de software. Queremos ajudar a criar a próxima geração de programadores — e um dos meios que oferecemos é se inscrever no curso MOOC Full Stack.',
-        'A Universidade de Helsinki e a Houston Inc. se uniram para desafiar tanto desenvolvedores individuais quanto empresas a aprender e a aprimorar suas habilidades no verdadeiro espírito Full Stack. Entre nossos afiliados e pioneiros do desafio estão Elisa Oyj, K-ryhmä, Konecranes Oyj, Terveystalo Oyj e a Unity Technologies Finland Ltd.',
+        'A Universidade de Helsinque e a Houston Inc. se uniram para desafiar tanto desenvolvedores individuais quanto empresas a aprender e a aprimorar suas habilidades no verdadeiro espírito Full Stack. Entre nossos afiliados e pioneiros do desafio estão Elisa Oyj, K-ryhmä, Konecranes Oyj, Terveystalo Oyj e a Unity Technologies Finland Ltd.',
         'Nosso objetivo é incentivar o aprendizado. Oferecemos um curso Full Stack completamente elaborado e com um escopo que diminui propositalmente a barreira de entrada para aprender sobre novas tecnologias de ponta e de qualidade de produção. O curso é gratuito e você pode participar de qualquer lugar e em qualquer hora, quando você desejar.',
         'O curso é construído por programadores para programadores e oferece valor tanto para iniciantes quanto para veteranos da indústria. O único pré-requisito é um bom entendimento dos fundamentos básicos da programação.',
       ]}
