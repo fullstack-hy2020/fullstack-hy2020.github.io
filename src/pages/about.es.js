505edd2c58024dd4f40f3cb6bd0b7ea820e880bd
Diego Oviedo <81838575+diegooviedo3004@users.noreply.github.com>
2023-04-06 16:54:23 -0600

    Update about.es.js

--- src/pages/about.es.js
+++ src/pages/about.es.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AboutPage from '../components/AboutPage';
 
 const About = () => {
-  return <AboutPage lang="es" title="About the course | Full Stack open" />;
+  return <AboutPage lang="es" title="Sobre el curso | Full Stack open" />;
 };
 
 export default About;
