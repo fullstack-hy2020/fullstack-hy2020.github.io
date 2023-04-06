76963a908960d54fefc6efb46c59548c46445be9
Diego Oviedo <81838575+diegooviedo3004@users.noreply.github.com>
2023-04-06 17:06:52 -0600

    Update challenge.es.js

--- src/pages/challenge.es.js
+++ src/pages/challenge.es.js
@@ -7,17 +7,17 @@ const Challenge = () => {
     <ChallengePage
       lang="es"
       title="Full stack -challenge | Full Stack open"
-      seoDescription="The University of Helsinki and Houston Inc. have partnered up to challenge both individual developers and companies alike to learn and build upon their expertise in true Full Stack development. The course is built by coders for coders, and offers something for both newcomers and seasoned industry veterans alike. "
+      seoDescription="La Universidad de Helsinki y Houston Inc. se han asociado para desafiar tanto a desarrolladores individuales como a empresas a aprender y mejorar sus habilidades en el verdadero desarrollo Full Stack. El curso está diseñado por codificadores para codificadores y ofrece algo tanto para principiantes como para veteranos experimentados de la industria. "
       aboutContent={[
-        'The world is in dire need of software developers. We want to help breed the next generation of coders — one of our offered methods is to enroll in the Full Stack MOOC course.',
-        'The University of Helsinki and Houston Inc. have partnered up to challenge both individual developers and companies alike to learn and build upon their expertise in true Full Stack spirit. Among our affiliates and challenge initiators are Elisa Oyj, K-ryhmä, Konecranes Oyj, Terveystalo Oyj, and Unity Technologies Finland Ltd.',
-        'Our objective is to encourage learning. We offer a fully-assembled and purposefully-scoped Full Stack course that lowers the barrier of entry for learning about new state-of-the-art and production grade technologies. The course is free of charge and you can participate from anywhere at anytime, at your leisure.',
-        'The course is built by coders for coders, and offers something for both newcomers and seasoned industry veterans alike. The only prerequisite is a solid grasp of basic programming fundamentals.',
+        'El mundo está en una necesidad urgente de desarrolladores de software. Queremos ayudar a criar la próxima generación de codificadores — Uno de nuestros métodos ofrecidos es inscribirse en el curso MOOC de Full Stack.',
+        'La Universidad de Helsinki y Houston Inc. se han asociado para desafiar tanto a desarrolladores individuales como a empresas a aprender y mejorar sus habilidades en el verdadero espíritu Full Stack. Entre nuestros afiliados e iniciadores de desafíos se encuentran Elisa Oyj, K-ryhmä, Konecranes Oyj, Terveystalo Oyj y Unity Technologies Finland Ltd.',
+        'Nuestro objetivo es fomentar el aprendizaje. Ofrecemos un curso Full Stack completamente ensamblado y diseñado con un propósito, que reduce la barrera de entrada para aprender sobre las últimas tecnologías de grado de producción. El curso es gratuito y puedes participar desde cualquier lugar en cualquier momento, a tu propio ritmo.',
+        'El curso está construido por codificadores para codificadores, y ofrece algo tanto para principiantes como para veteranos experimentados de la industria. El único requisito previo es tener un sólido conocimiento de los fundamentos básicos de la programación.',
       ]}
       joinContent={[
-        'By accepting the challenge you are encouraging and empowering the members of your working community to acquire new skills. The fully-online Full Stack course will make a great addition to your internal training and development programs and will serve well as self-study material amongst your other in-company training offerings.',
-        'By partaking in the challenge, you will send a strong message to the outside world about your company’s technical expertise — this will help build your employer image and increase your visibility to software industry professionals.',
-        'Joining the challenge is free of charge. Companies that heed the call and accept the challenge will have their logo proudly placed on the Full Stack course webpage.',
+        'Al aceptar el desafío, estás alentando y capacitando a los miembros de tu comunidad laboral para adquirir nuevas habilidades. El curso Full Stack completamente en línea será una gran adición a tus programas internos de capacitación y desarrollo, y servirá bien como material de autoestudio entre otras ofertas de capacitación interna de tu empresa.',
+        'Al participar en el desafío, enviarás un mensaje claro al mundo exterior sobre la experiencia técnica de tu empresa — lo que ayudará a construir tu imagen como empleador y aumentará tu visibilidad entre los profesionales de la industria del software.',
+        'Unirse al desafío es gratuito. Las empresas que atiendan al llamado y acepten el desafío tendrán su logotipo colocado con orgullo en la página web del curso Full Stack.',
       ]}
     />
   );
