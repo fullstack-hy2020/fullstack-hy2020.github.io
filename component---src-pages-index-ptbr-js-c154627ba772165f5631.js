965cc55dd5306aefc3e5cad66b36ddc6a66f1ec7
vejol <vejol@users.noreply.github.com>
2025-03-02 18:28:37 +0000

    deployed via Gatsby Publish Action 🎩 for 448d512f811eb055689688b3950cef729487394b

---
+++ component---src-pages-index-ptbr-js-c154627ba772165f5631.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{r2np:function(n,a,e){"use strict";e.r(a);var t=e("q1tI"),p=e.n(t),r=e("UQMq");a.default=()=>p.a.createElement(r.a,{lang:"ptbr"})}}]);
+//# sourceMappingURL=component---src-pages-index-ptbr-js-c154627ba772165f5631.js.map
