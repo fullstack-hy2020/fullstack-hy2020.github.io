bf5f46ada6647b84e36d9387ee9fd1bf59b03b28
mluukkai <mluukkai@users.noreply.github.com>
2025-06-29 11:59:53 +0000

    deployed via Gatsby Publish Action 🎩 for c86c810f861fe8308908b617c5b4f318e52fc015

---
+++ component---src-pages-search-js-6a7301a08fa1e893839d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{Bxyr:function(a,t,e){"use strict";e.r(t);var n=e("q1tI"),l=e.n(n),i=e("D9jW");t.default=a=>{let{data:t}=a;return l.a.createElement(i.a,{localSearch:t.localSearchFinnish,title:"Etsi hakusanalla materiaalista",inputPlaceholder:"Syötä hakusana",lang:"fi"})}}}]);
+//# sourceMappingURL=component---src-pages-search-js-6a7301a08fa1e893839d.js.map
