7d864d55e97f57ce28fc2ff6f2f64709193ec26c
Ahmad <ahmadkhaefi@outlook.com>
2025-11-17 16:01:22 +0330

    Add RTL compatibility

--- src/components/Navigation/SearchLink/index.js
+++ src/components/Navigation/SearchLink/index.js
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 import SearchIcon from '../../SearchIcon';
 import SrOnly from '../../SrOnly';
+import '../../common.scss'
 import styles from './SearchLink.module.scss';
 
 const getLinkTo = (lang) => {
@@ -14,7 +15,10 @@ const SearchLink = ({ lang }) => {
   const { t } = useTranslation();
 
   return (
-    <Link to={getLinkTo(lang)} className={styles.searchLink}>
+    <Link
+    to={getLinkTo(lang)}
+    className={styles.searchLink}
+    >
       <SrOnly>{t('navigation:searchLinkSrLabel')}</SrOnly>
       <SearchIcon />
     </Link>
